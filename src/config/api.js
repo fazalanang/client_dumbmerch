53b96f58583142e3492e433872160c914ec34d0a
Your Name <faza.lanangdjati@gmail.com>
2022-06-10 01:59:23 +0700

    client_dumbmerch

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,22 @@
-import axios from 'axios';
+export const API = () => {
+  const baseUrl =
+    process.env.REACT_APP_SERVER_URL ||
+    'https://dumbmerch-34.herokuapp.com/api/v1' ||
+    'https://localhost:5000/api/v1';
 
-// Create base URL API
-export const API = axios.create({
-  baseURL: 'http://localhost:5000/api/v1/',
-});
+  const executeAPI = async (endpoint, config) => {
+    const response = await fetch(baseUrl + endpoint, config);
+    const data = await response.json();
+    return data;
+  };
+
+  return {
+    get: executeAPI,
+    post: executeAPI,
+    patch: executeAPI,
+    delete: executeAPI,
+  };
+};
 
 // Set Authorization Token Header
 export const setAuthToken = (token) => {
